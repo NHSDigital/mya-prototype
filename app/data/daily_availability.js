5f6005dccec7e33ab14e6b3e3c44f67ae09306d0
Jon Roobottom <=>
2025-10-22 17:35:02 +0100

    updated prototype homepage

--- app/data/daily_availability.js
+++ app/data/daily_availability.js
@@ -74,5 +74,24 @@ module.exports = {
         capacity: 1
       }
     ]
+  },
+  '2025-10-20': {
+    date: '2025-10-20',
+    site_id: 1,
+    sessions: [
+      {
+        from: '09:30',
+        until: '13:00',
+        services: ['COVID:18+', 'FLU:18-64', 'FLU:65+', 'FLU_COVID:18-64', 'FLU_COVID:65+'],
+        slotLength: 10,
+        capacity: 1
+      }, {
+        from: '14:00',
+        until: '17:00',
+        services: ['COVID:18+', 'FLU:18-64', 'FLU:65+', 'FLU_COVID:18-64', 'FLU_COVID:65+'],
+        slotLength: 10,
+        capacity: 1
+      }
+    ]
   }
 }
